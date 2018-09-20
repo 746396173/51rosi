1fb87088db807d353b8ea0a69c208de1b04e7dfb
zhangxiang <zhangxiang@zhangxiangtekiMacBook-Pro.local>
2018-09-20 22:08:05 +0800

    1

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -8,8 +8,8 @@
 
 namespace app\index\controller;
 
+use think\Cache;
 use think\Controller;
-use think\Db;
 use think\Request;
 use app\index\model\Site as SiteModel;
 use app\admin\model\Linkship as LinkModel;
@@ -20,8 +20,14 @@ class Base extends Controller
     protected $links = [];
     public function __construct(Request $request = null)
     {
-        $this->site = SiteModel::get(0)->cache(true,600);
-        $this->links = Db::cache(true,600)->select();
+        $this->site = SiteModel::get(0)->cache(true,600)->find();
+        if (Cache::get('link')){
+            $this->links = Cache::get('link');
+        }else{
+            $this->links = LinkModel::all();
+            Cache::set('link',$this->links);
+        }
+
         parent::__construct($request);
         $this->assign('links',$this->links);
         $this->assign('site',$this->site);
